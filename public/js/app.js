bf21f25d98aff1b977cf4402bf41606d900e364e
Patrick Kaserer <patrick.kaserer@web.de>
2020-08-18 11:13:02 +0200

    WiP: add styleguide component

--- public/js/app.js
+++ public/js/app.js
@@ -65899,8 +65899,8 @@ if (document.getElementById('example')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\Blind_MeetUp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Blind_MeetUp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\xampp\htdocs\Blind_MeetUp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\xampp\htdocs\Blind_MeetUp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/styleguide.blade.php
+++ resources/views/styleguide.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    <div id="example"></div>
                     Hier kommt der Styleguide
                 </div>
             </div>
