81c843b6a94b9d591cb31a91b9bfd7a17a42ed77
doldsimo <simon.dold@hs-furtwangen.de>
2020-10-21 21:17:28 +0200

    TASK: translate footer

--- resources/js/components/Footer/Footer.js
+++ resources/js/components/Footer/Footer.js
@@ -5,34 +5,43 @@ import Wortmarke_OneLine_invertiert_cut from '../../../assets/img/Wortmarke_OneL
 import FlagDE from '../../../assets/img/flags/flag-de.svg';
 import FlagEN from '../../../assets/img/flags/flag-en.svg';
 
+import { withNamespaces } from 'react-i18next';
+import i18n from '../../i18next';
+
 class Footer extends Component {
+
+    changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    }
+
     render() {
+        const { t } = this.props;
         return (
             <div className="footer container-fluid">
                 <div className="row">
                     <div className="col-lg-7">
-                        <p className="footer__heading">Informationen</p>
-                        <FooterButton href link="https://www.hs-furtwangen.de/">Anmelden</FooterButton>
-                        <FooterButton link="/feedback">Feedback</FooterButton>
-                        <FooterButton link="/kontakt">Kontakt</FooterButton>
-                        <FooterButton href link="https://www.hs-furtwangen.de/">Hochschule Furtwangen</FooterButton>
+                        <p className="footer__heading">{t('Informations')}</p>
+                        <FooterButton href link="https://www.hs-furtwangen.de/">{t('SignIn')}</FooterButton>
+                        <FooterButton link="/feedback">{t('Feedback')}</FooterButton>
+                        <FooterButton link="/kontakt">{t('Contact')}</FooterButton>
+                        <FooterButton href link="https://www.hs-furtwangen.de/">{t('HochschuleFurtwangen')}</FooterButton>
                     </div>
                     <div className="col-lg-5">
-                        <p className="footer__heading">Rechtliches</p>
-                        <FooterButton link="/impressum">Impressum</FooterButton>
-                        <FooterButton link="/datenschutz">Datenschutz</FooterButton>
-                        <FooterButton link="/richtlinien">Richtlinien</FooterButton>
+                        <p className="footer__heading">{t('Legal')}</p>
+                        <FooterButton link="/impressum">{t('Impressum')}</FooterButton>
+                        <FooterButton link="/datenschutz">{t('DataProtection')}</FooterButton>
+                        <FooterButton link="/richtlinien">{t('Guidelines')}</FooterButton>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-lg-7 col-8" >
                         <img src={Wortmarke_OneLine_invertiert_cut} className="footer__logo" alt="BlindMeetUp_Logo"></img>
-                        <p className="footer__text">© 2020 Blind MeetUp. Alle Rechte vorbehalten. Ein Semesterprojekt unterstützt durch die Hochschule Furtwangen.</p>
+                        <p className="footer__text">{t('AllRightsReserved')}</p>
                     </div>
                     <div className="col-lg-5 col-4">
                         <div className="footer__flags">
-                            <img src={FlagDE} className="footer__flag" alt="BlindMeetUp_flag_de" style={{ width: "200px" }}></img>
-                            <img src={FlagEN} className="footer__flag" alt="BlindMeetUp_flag_en" style={{ width: "200px" }}></img>
+                            <img src={FlagDE} onClick={() => this.changeLanguage('de')} className="footer__flag" alt="BlindMeetUp_flag_de" style={{ width: "200px" }}></img>
+                            <img src={FlagEN} onClick={() => this.changeLanguage('en')} className="footer__flag" alt="BlindMeetUp_flag_en" style={{ width: "200px" }}></img>
                         </div>
                     </div>
                 </div>
@@ -42,4 +51,4 @@ class Footer extends Component {
     }
 }
 
-export default Footer;
+export default withNamespaces('footer')(Footer);
