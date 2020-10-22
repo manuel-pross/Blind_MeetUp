395f066bd11409e8630d57d73a9a83cfc722b361
doldsimo <simon.dold@hs-furtwangen.de>
2020-10-22 12:55:59 +0200

    TASK: adding translation

--- resources/js/containers/Pages/Impressum.js
+++ resources/js/containers/Pages/Impressum.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import { withNamespaces } from 'react-i18next';
 
-const Impressum = () => (
+
+const Impressum = ({ t }) => (
     <React.Fragment>
         <Navbar />
-        <div style={{ paddingTop: "100px" }}>
-            Impressum
+        <div className="container" style={{ marginTop: '100px' }}>
+            <h1>{t('Heading')}</h1 >
+            <p>Die nachstehenden Informationen enthalten die gesetzlich vorgesehenen Pflichtangaben zur
+                Anbieterkennzeichnung und weitere Hinweise zur Internetpräsenz „www.blindmeetup.hs-furtwangen.de“  </p>
+            <p>{t('Welcome to React')}</p>
         </div>
-    </React.Fragment>
+        <Footer />
+    </React.Fragment >
 );
 
-export default Impressum;
+export default withNamespaces('impressum')(Impressum);
