b128f15d0667ede7627619cd6f180358fe87d7ec
Patrick Kaserer <patrick.kaserer@web.de>
2020-12-10 14:56:40 +0100

    WIP: add pastMeeting

--- resources/js/components/MeetUps/pastMeeting.js
+++ resources/js/components/MeetUps/pastMeeting.js
@@ -6,27 +6,101 @@ import smileyBad from '../../../assets/img/icons/smiley-bad.svg';
 
 import { withTranslation } from 'react-i18next';
 
+const opacity05 = { opacity: ".7" };
+const opacity1 = { opacity: "1" };
 class PastMeeting extends Component {
 
+
+   state = {
+      btnPressed: false,
+      displayBeforeClick: { display: "block" },
+      displayAfterClick: { display: "none" },
+      pastMeetingClass: "pastMeeting",
+      gooodSmileyOp: opacity05,
+      neutralSmileyOp: opacity05,
+      badSmileyOp: opacity05
+   }
+
+   pastMeetingClickHandler = () => {
+      if (!this.state.btnPressed) {
+         this.setState({
+            displayAfterClick: { display: "flex" },
+            displayBeforeClick: { display: "none" }
+         })
+      }
+
+      if (this.state.btnPressed) {
+         this.setState({ pastMeetingClass: "pastMeeting--closed" })
+         setTimeout(() => {
+            document.querySelector(".pastMeeting--closed").parentElement.parentElement.remove();
+         }, 1100);
+      }
+   }
+
+
+   smileyClickHandler = (smiley) => {
+      this.setSmileyOnDefault();
+      this.setState({
+         btnPressed: true,
+      })
+      switch (smiley) {
+         case "good":
+            this.setState({ gooodSmileyOp: opacity1 })
+            break;
+         case "neutral":
+            this.setState({ neutralSmileyOp: opacity1 })
+            break;
+         default:
+            this.setState({ badSmileyOp: opacity1 })
+      }
+   }
+
    exitClickHandler = () => {
-     //
+      this.setSmileyOnDefault();
+      this.setState({
+         btnPressed: false,
+         displayAfterClick: { display: "none" },
+         displayBeforeClick: { display: "block" }
+      })
+   }
+
+   setSmileyOnDefault = () => {
+      this.setState({
+         gooodSmileyOp: { opacity: ".7" },
+         neutralSmileyOp: { opacity: ".7" },
+         badSmileyOp: { opacity: ".7" }
+      })
    }
 
    render() {
       const { t } = this.props;
       return (
-         <div className="pastMeeting">
-            <button onClick={this.exitClickHandler} className="meeting__exitBtn"></button>
-            <div className="pastMeeting__textWrapper">
-               <p className="pastMeeting__text">{t("satisfiedDesc1")}</p>
-               <p className="pastMeeting__text">{t("satisfiedDesc2")}</p>
+         <div className={this.state.pastMeetingClass}>
+            <div className="pastMeeting__topWrapper">
+               <div className="pastMeeting__wrapperBeforeClick" style={this.state.displayBeforeClick}>
+                  <p className="pastMeeting__notRated">{t("notRated")}</p>
+                  <div className="pastMeeting__dataWrapper">
+                     <p className="pastMeeting__data">{this.props.day}</p>
+                     <p className="pastMeeting__data pastMeeting__data--date">{this.props.date + ", "} {this.props.time}</p>
+                  </div>
+                  <div className="pastMeeting__dataWrapperLocation">
+                     <p className="pastMeeting__data pastMeeting__data--place">{this.props.place}</p>
+                  </div>
+               </div>
+               <div className="pastMeeting__wrapperAfterClick" style={this.state.displayAfterClick}>
+                  <button onClick={this.exitClickHandler} style={this.state.displayAfterClick} className="meeting__exitBtn"></button>
+                  <div className="pastMeeting__textWrapper">
+                     <p className="pastMeeting__text">{t("satisfiedDesc1")}</p>
+                     <p className="pastMeeting__text">{t("satisfiedDesc2")}</p>
+                  </div>
+                  <div className="pastMeeting__smileyWrapper" >
+                     <img onClick={() => { this.smileyClickHandler("good") }} style={this.state.gooodSmileyOp} className="pastMeeting__smiley" src={smileyGood} ></img>
+                     <img onClick={() => { this.smileyClickHandler("neutral") }} style={this.state.neutralSmileyOp} className="pastMeeting__smiley" src={smileyNeutral} ></img>
+                     <img onClick={() => { this.smileyClickHandler("bad") }} style={this.state.badSmileyOp} className="pastMeeting__smiley" src={smileyBad} ></img>
+                  </div>
+               </div>
             </div>
-            <div className="pastMeeting__smileyWrapper" >
-               <img className="pastMeeting__smiley" src={smileyGood} ></img>
-               <img className="pastMeeting__smiley" src={smileyNeutral} ></img>
-               <img className="pastMeeting__smiley " src={smileyBad} ></img>
-            </div>
-            <div className="pastMeeting__btnWrapper" style={{ textAlign: "center" }}>
+            <div onClick={this.pastMeetingClickHandler} className="pastMeeting__btnWrapper" style={{ textAlign: "center" }}>
                <button className="btn">{t("btnRate")}</button>
             </div >
          </div>
