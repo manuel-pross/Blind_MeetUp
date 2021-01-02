a1533f2e4b48199304dc879b668d79943c165d22
doldsimo <simon.dold@hs-furtwangen.de>
2021-01-02 18:38:00 +0100

    TASK: update webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .setResourceRoot("https://blind-meetup.dm.hs-furtwangen.de/Blind_MeetUp/public/");
     // .sourceMaps(true, 'source-map')
-    // .setResourceRoot("https://blind-meetup.dm.hs-furtwangen.de/Blind_MeetUp/public/");
