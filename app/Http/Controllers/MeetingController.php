3bf5c2250ab16c8441e7f49c9a8e89385ca4bde5
doldsimo <simon.dold@hs-furtwangen.de>
2021-01-04 13:06:32 +0100

    FIX: update middleware

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\DB;
 class MeetingController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     public function index() {
         return Meeting::all();
     }
