c5626fae2b12c86c54ec38841e764fb51d830b5e
doldsimo <simon.dold@hs-furtwangen.de>
2021-01-05 10:32:03 +0100

    FIX: footer buttons

--- resources/js/components/Footer/FooterButton/FooterButton.js
+++ resources/js/components/Footer/FooterButton/FooterButton.js
@@ -16,17 +16,17 @@ class Footer extends Component {
         if (this.props.href) {
             button = (
                 <a href={this.props.link} target="_blank" rel="noopener noreferrer">
-                    <button className="btn btn-second mr-100 footer__button">{this.props.children}</button>
+                    <button className="btn btn-second mr-100 mb-md-100 footer__button">{this.props.children}</button>
                 </a >
             );
         } else if (this.props.signout) {
             button = (
-                <Link to={this.props.link} onClick={this.handleLogout}><button className="btn btn-second mr-100 footer__button">{this.props.children}</button></Link>
+                <Link to={this.props.link} onClick={this.handleLogout}><button className="btn btn-second mr-100 mb-md-100 footer__button">{this.props.children}</button></Link>
             );
         }
         else {
             button = (
-                <Link to={this.props.link}><button className="btn btn-second mr-100 footer__button">{this.props.children}</button></Link>
+                <Link to={this.props.link}><button className="btn btn-second mr-100 mb-md-100 footer__button">{this.props.children}</button></Link>
             );
         }
 
