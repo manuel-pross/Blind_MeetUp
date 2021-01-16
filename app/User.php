3a998d96586f06162d7b5993be707248112459e9
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2021-01-16 08:31:14 +0100

    TASK: pendingMeetings just returns meetings with free slots for joining

--- app/User.php
+++ app/User.php
@@ -35,6 +35,7 @@ class User extends Authenticatable
 
     public function pendingMeetings() {
         return $this->belongsToMany('App\Meeting')
+        ->whereRaw('members < max_members')
         ->withPivot(['status'])
         ->where('status', 'pending');
     }
